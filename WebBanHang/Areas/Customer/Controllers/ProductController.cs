75d0a5f127d795677e6c006a44c3cf01d19cbf90
Chung <chungnguyenthi1010@gmail.com>
2025-06-02 10:56:45 +0700

    XemSP

--- WebBanHang/Areas/Customer/Controllers/ProductController.cs
+++ WebBanHang/Areas/Customer/Controllers/ProductController.cs
@@ -18,9 +18,10 @@ namespace WebBanHang.Areas.Customer.Controllers
         public IActionResult Index(int catid=1)
         {
             var dsLoai = _db.Categories.OrderBy(x => x.DisplayOrder).Select(c=>new CategoryNew { Id = c.Id, Name = c.Name, TotalProduct = _db.Products.Where(p => p.CategoryId == c.Id).Count() }).ToList();
-
+            var catName = _db.Categories.Find(catid).Name;
             var dsSanPham = _db.Products.Where(p => p.CategoryId == catid).ToList();
             ViewBag.DSLOAI = dsLoai;
+            ViewBag.CATEGORY_NAME = catName;
             return View(dsSanPham);
         }
     }
