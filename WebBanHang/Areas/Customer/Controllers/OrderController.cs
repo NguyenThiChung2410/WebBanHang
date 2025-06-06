200162d227a0430e046a59d8a37df87a3b7e03a5
Chung <chungnguyenthi1010@gmail.com>
2025-06-06 08:52:58 +0700

    HT

--- WebBanHang/Areas/Customer/Controllers/OrderController.cs
+++ WebBanHang/Areas/Customer/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,8 @@ using WebBanHang.Models;
 
 namespace WebBanHang.Areas.Customer.Controllers
 {
+    
+    [Area("Customer")]
     public class OrderController : Controller
     {
         private readonly ApplicationDbContext _db;
